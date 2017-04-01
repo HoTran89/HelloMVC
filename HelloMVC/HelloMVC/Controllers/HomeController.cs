23164a6c1de0992ce06bf0d6ead1cea1b5d607dc
Ho Tran <tran.ngoc.ho89@gmail.com>
2017-04-01 13:55:02 +0700

    init project

---
+++ HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "this is viewbage about";
+            ViewBag.HoTran = "Ho Tran";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
